9721d0a62f1476b3fe15b1a28fe04df3ae155db9
preston <polston13@outlook.com>
2024-09-15 10:57:52 -0400

    made the light turn on or off depending on a boolean value there is still an issue with holding down the button but it does turn on and off with one click

--- arduinoPushButton/arduinoPushButton.ino
+++ arduinoPushButton/arduinoPushButton.ino
@@ -1,9 +1,43 @@
+int buttonPin = 2;
+int redPin = 3;
+int buttonValue;
+int br = 115200;
+int dt = 250;
+bool light = false;
+
+
+
 void setup() {
-
-
+Serial.begin(br);
+pinMode(buttonPin, INPUT);
+pinMode(redPin, OUTPUT);
 }
 
 void loop() {
-
+buttonValue = digitalRead(buttonPin);
+Serial.println(buttonValue);
+while(buttonValue == 0){
+  Serial.println("Light is "+light);
+if(light == false){
+  Serial.println("its now true");
+  light = true;
+  buttonValue = 1;
+  delay(dt);
+  while(buttonValue == 1){
+  digitalWrite(redPin, HIGH);
+  buttonValue = digitalRead(buttonPin);
+  }
+}
+if(light == true){
+  Serial.println("its now false");
+  light = false;
+  buttonValue = 1;
+  delay(dt);
+  while(buttonValue == 1){
+  digitalWrite(redPin, LOW);
+  buttonValue = digitalRead(buttonPin);
+  }
+}
+}
 
 }
